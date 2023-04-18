c01e44f1284baa5ed7d6d75dae4dad17ee27bb72
David Dalzon <123088706+DDalzon@users.noreply.github.com>
2023-04-18 17:25:54 -0300

    Setting the player movement
    Implemented coyote time and jump buffering.

--- Project B/Assets/Scripts/PlayerController.cs
+++ Project B/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,16 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] float jumpForce;
 	float moveInput;
 	
+	[SerializeField] float coyoteTime = 0.065f;
+	float coyoteTimeCounter;
+	
+	[SerializeField] float jumpBufferTime = 0.15f;
+	float jumpBufferTimeCounter;
+	
+	bool facingRight = true;
+	[SerializeField] Collider2D feetCollider;
+	[SerializeField] LayerMask groundLayer;
+	
 	Rigidbody2D rb;
 	
 	
@@ -19,11 +29,82 @@ public class PlayerController : MonoBehaviour
 	void Update() 
 	{
 		moveInput = Input.GetAxisRaw("Horizontal");
+		
+		Jump();
+		SetCoyoteTime();
+		
+		if(Input.GetButtonDown("Jump"))
+		{
+			jumpBufferTimeCounter = jumpBufferTime;
+		}else
+		{
+			jumpBufferTimeCounter -= Time.deltaTime;
+		}
 	}
 	
 	void FixedUpdate() 
 	{
-		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+		MoveAndFlip();
 	}
 	
+	void MoveAndFlip()
+	{
+		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+		
+		if(!facingRight && moveInput > 0)
+		{
+			Flip();
+		} else if (facingRight && moveInput < 0)
+		{
+			Flip();
+		}
+	}
+	
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector2 scaler = transform.localScale;
+		scaler.x *= -1;
+		transform.localScale = scaler;
+	}
+	
+	void Jump()
+	{
+		if(coyoteTimeCounter > 0f && jumpBufferTimeCounter > 0f)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+			jumpBufferTimeCounter = 0f;
+		}
+		
+		if(Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y*0.5f);
+			
+			coyoteTimeCounter = 0f; // isso aqui é pro cara n pular 2x spamando o botao de jump
+		}
+	}
+	
+	void SetCoyoteTime()
+	{
+		if(IsGrounded())
+		{
+			coyoteTimeCounter = coyoteTime;
+		}
+		else
+		{
+			coyoteTimeCounter -= Time.deltaTime;
+		}
+	}
+	
+	bool IsGrounded()
+	{
+		return feetCollider.IsTouchingLayers(groundLayer);
+	}
+	/* 
+		O que fazer no código:
+		-Fazer com q o movimento no ar seja por AddForce;
+	
+		Bugs conhecidos:
+		- o pulo bufferado sempre vai com força total se o player soltar o botao de pulo rapido;
+	*/
 }
